bc8823a56891bfa6b4e6b25ce33b4e4c85ac88ba
Nova Andre Saputra <novaandre.saputraadit@gmail.com>
2022-10-13 11:11:18 +0700

    ONG: adding navbar button logout view in homepage

---
+++ app/src/main/java/com/uwika/traveljournal/HomeActivity.java
@@ -0,0 +1,30 @@
+package com.uwika.traveljournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        toolbar = findViewById(R.id.home_appbar);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+}
