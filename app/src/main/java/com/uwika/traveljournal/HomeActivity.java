3678b1e0fdb039200f505f13d90dc3c38f365998
Nova Andre Saputra <novaandre.saputraadit@gmail.com>
2022-10-19 13:48:33 +0700

    ONG: adding mapbox and recycleview

--- app/src/main/java/com/uwika/traveljournal/HomeActivity.java
+++ app/src/main/java/com/uwika/traveljournal/HomeActivity.java
@@ -6,19 +6,31 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+;import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
+import com.mapbox.maps.plugin.Plugin;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private MapView mapView;
+    private static final String[] COUNTRIES = new String[]{"Sidoarjo", "Surabaya", "Jombang", "Blitar"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         toolbar = findViewById(R.id.home_appbar);
-
         setSupportActionBar(toolbar);
+
+        mapView = findViewById(R.id.mapbox_home);
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
+
+        AutoCompleteTextView autoCTxtV_journals = findViewById(R.id.autoCTxtV_journals);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.custom_list_item, R.id.txtV_list_item, COUNTRIES);
+        autoCTxtV_journals.setAdapter(adapter);
     }
 
     @Override
@@ -26,5 +38,29 @@ public class HomeActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }
--- app/src/main/java/com/uwika/traveljournal/LoginActivity.java
+++ app/src/main/java/com/uwika/traveljournal/LoginActivity.java
@@ -1,14 +1,30 @@
 package com.uwika.traveljournal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
+    AppCompatButton btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
